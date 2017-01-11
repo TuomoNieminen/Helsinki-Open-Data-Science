ab02dee8051c0e0ceeec6fd7660f0905ef76b6b5
TuomoNieminen <tuomo.a.nieminen@helsinki.fi>
2017-01-11 14:12:18 +0200

    add learning2014 data

---
+++ data/create_learning2014.R
@@ -0,0 +1,15 @@
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+lrn14$attitude <- lrn14$Attitude / 10
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D07","D14","D22","D30")
+lrn14$deep <- rowMeans(lrn14[, deep_questions])
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+lrn14$surf <- rowMeans(lrn14[, surface_questions])
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+lrn14$stra <- rowMeans(lrn14[, strategic_questions])
+learning2014 <- lrn14[, c("gender","Age","attitude", "deep", "stra", "surf", "Points")]
+
+colnames(learning2014)[2] <- "age"
+colnames(learning2014)[7] <- "points"
+learning2014 <- learning2014[learning2014$points > 0, ]
+write.table(file = "learning2014.txt", learning2014)
+
