3760c9e5baff26cc8e5b16996edd89f0c2db1938
Tuomo Nieminen <tuomo.a.nieminen@helsinki.fi>
2017-01-23 11:46:05 +0200

    no change

--- datasets/create_learning2014.R
+++ datasets/create_learning2014.R
@@ -10,7 +10,9 @@ learning2014 <- lrn14[, c("gender","Age","attitude", "deep", "stra", "surf", "Po
 
 colnames(learning2014)[2] <- "age"
 colnames(learning2014)[7] <- "points"
+
 learning2014 <- learning2014[learning2014$points > 0, ]
+
 write.table(learning2014, file = "learning2014.txt", sep = ",", row.names = F)
 
 # learning2014 <- read.table("learning2014.txt", sep = ",", header = T))
