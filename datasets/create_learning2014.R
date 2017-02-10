6dbcc6abfa2766e31e4db1bbb51c3c78be179a3e
Tuomo Nieminen <tuomo.a.nieminen@helsinki.fi>
2017-02-10 13:07:02 +0200

    add learning2014 meta info
    (from rmvd learning2014.R)

--- datasets/create_learning2014.R
+++ datasets/create_learning2014.R
@@ -2,6 +2,17 @@
 # Tuomo Nieminen 2017
 # Data from Kimmo Vehkalahti
 
+# # meta
+# browseURL("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-meta.txt")
+# 
+# # summamuuttujat
+# browseURL("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS2-meta.txt")
+# 
+# # Kimmon analyyseja
+# browseURL("http://www.helsinki.fi/~kvehkala/JYTmooc/Abstract-Vehkalahti.pdf")
+# browseURL("http://www.helsinki.fi/~kvehkala/JYTmooc/Kimmo_Vehkalahti_ISI60.pdf")
+# 
+
 # read data
 lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
 
